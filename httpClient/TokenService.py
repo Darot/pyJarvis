1eaca6ba7979fa21073eb9ac256fbab4bde99043
Daniel Roth <daan.roth@googlemail.com>
2017-08-03 23:45:21 +0200

    finally accessed api for the first time

--- httpClient/TokenService.py
+++ httpClient/TokenService.py
@@ -9,4 +9,9 @@ os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 def fetch_token():
     client = BackendApplicationClient(client_id="jhipsterCoreapp")
     oauth = OAuth2Session(client=client)
-    return oauth.fetch_token(token_url="http://localhost:8080/oauth/token?grant_type=password", client_id="jhipsterCoreapp", client_secret="my-secret-token-to-change-in-production", password="admin", username="admin")
+    oauth.fetch_token(token_url="http://localhost:8080/oauth/token?grant_type=password",
+                      client_id="jhipsterCoreapp",
+                      client_secret="my-secret-token-to-change-in-production",
+                      password="admin",
+                      username="admin")
+    print oauth.get("http://localhost:8080/api/users?page=0&size=20&sort=id,asc")._content
